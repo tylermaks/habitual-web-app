fa482ea6341d11ae390bea335edf7924d5f56d81
Tyler Maksymiw <tmaksymiw@MacBook-Air.local>
2024-10-31 21:51:30 -0400

    Add habit grid html structure

--- src/components/dashboard/HabitGrid.tsx
+++ src/components/dashboard/HabitGrid.tsx
@@ -1,4 +1,9 @@
+"use client"
+
+import { useState } from "react"
+
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 export default function HabitGrid(){ 
 
@@ -58,9 +63,39 @@ export default function HabitGrid(){
 
 
     return ( 
-        <>
-            {fullYearGrid()}
+        <table className="p-4 border border-spacing-[3px] border-separate overflow-hidden">
+          <thead>
+            <tr className="h-[13px]">
+                <td className="w-7">
+                    <span className="sr-only">Day of the Week</span>
+                </td>
+                { Array.from({length: 12}, (_, index) => (
+                    <td key={index} colSpan={4}>
+                        <span className="text-white text-sm">{months[index].slice(0, 3)}</span>
+                    </td>
+                ))}
+            </tr>
+            <tr></tr>
+          </thead>
+          <tbody>
+                {daysOfTheWeek.map((day, index) => { 
+                    return (
+                        <tr key={index} className="h-3">
+                            <td className="w-7 pr-8 relative">
+                                <span className={(index) % 2 ? "text-sm absolute bottom-[-3px] text-white" : "sr-only"}>{day.slice(0, 3)}</span>
+                            </td>
+                            { Array.from({length: 52}, (_, index) => (
+                                <td key={index} className="w-3 bg-surface-a10 rounded-sm table-cell border-separate">
 
-        </>
+                                </td>
+                            ))}
+                        </tr>
+                    )
+                })}
+                
+                
+            </tbody>
+ 
+        </table>
     );
 };
