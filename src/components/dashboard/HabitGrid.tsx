0b9782aa36be6302f9ac3b93db1275688658ff9b
Tyler Maksymiw <tmaksymiw@MacBook-Air.local>
2024-11-07 20:46:53 -0500

    Update grid component to include hover and contribution functions

--- src/components/dashboard/HabitGrid.tsx
+++ src/components/dashboard/HabitGrid.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
+import { start } from "repl"
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -12,25 +13,32 @@ export default function HabitGrid(){
     //Use for colSpan of months on table component
     const weeksInMonth = (monthID: number) => {
         const year = new Date().getFullYear();
+        const dayOffset = new Date(year, monthID - 1, 1).getDay() + 1;
         const daysInMonth = new Date(year, monthID, 0 ).getDate();
 
-        return Math.floor(daysInMonth / 7);
+        return Math.floor((daysInMonth + dayOffset) / 7);
     }
 
     //Assign date to each grid cell
     const getGridDate = (dayIndex: number, weekIndex: number) => {
         const year = 2024
-        const date = new Date(year, 0)
+        const date = new Date(year, 0)    
         const dayOfYear = (weekIndex * 7 ) + dayIndex
+
+        
         
         date.setDate(dayOfYear)
+
         return date.toLocaleDateString()
     }
 
     //Used on hover of grid cell
     const displayProgressInfo = (date: string) => { 
-        console.log("WORKING")
         setHoverID(date)
+    }
+
+    const restetProgressInfo = () => { 
+        setHoverID("")
     }
 
 
@@ -81,6 +89,45 @@ export default function HabitGrid(){
                             ]
                         }
                     ]
+                },
+                {
+                    dayID: 1,
+                    date: "7/12/2024", 
+                    habit: [ 
+                        {
+                            habitID: 1, 
+                            habitTitle:"Get in shape", 
+                            tasksArr: [
+                                { 
+                                    taskID: 1, 
+                                    taskTitle: "Buy new shoes",
+                                    complete: false,
+                                    completeData: "", 
+                                    notes:""
+                                }, 
+                                { 
+                                    taskID: 2, 
+                                    taskTitle: "Find gym", 
+                                    complete: false,
+                                    completeData: "", 
+                                    notes: ""
+                                }
+                            ]
+                        },
+                        {
+                            habitID: 2, 
+                            habitTitle:"Study!", 
+                            tasksArr: [
+                                { 
+                                    taskID: "2-1", 
+                                    taskTitle: "Stuy for test",
+                                    complete: false,
+                                    completeData: "", 
+                                    notes:""
+                                }, 
+                            ]
+                        }
+                    ]
                 }
             ]
         }
@@ -91,9 +138,9 @@ export default function HabitGrid(){
     
         for (let obj in dailyProgressData){ 
             const targetCell = document.querySelector(`td[data-date="${dailyProgressData[obj].date}"]`)
-            const colorValue = Math.min(dailyProgressData[obj].habit.length * 100, 900)
-           
+            
             if (targetCell) {
+                const colorValue = Math.min(dailyProgressData[obj].habit.length * 100 + 100, 900)
                 targetCell.className = `bg-green-${colorValue} w-3 rounded-sm table-cell border-separate relative`;
             }
         }
@@ -106,7 +153,7 @@ export default function HabitGrid(){
 
 
     return ( 
-        <table className="p-4  border-spacing-[3px] border-separate overflow-hidden">
+        <table className="p-4  border-spacing-[3px] border-separate">
           <thead>
             <tr className="h-[13px]">
                 <td className="w-7">
@@ -127,23 +174,41 @@ export default function HabitGrid(){
                             <td className="w-7 pr-8 relative">
                                 <span className={(dayIndex) % 2 ? "text-sm absolute bottom-[-3px] text-white" : "sr-only"}>{day.slice(0, 3)}</span>
                             </td>
-                            { Array.from({length: 52}, (_, weekIndex) => (
-                                <td 
-                                    className="w-3 bg-surface-a10 rounded-sm table-cell border-separate relative"
-                                    onMouseEnter={() => displayProgressInfo(getGridDate(dayIndex, weekIndex))}
-                                    key={weekIndex} 
-                                    id={`${dayIndex}-${weekIndex}`}  
-                                    data-date={getGridDate(dayIndex, weekIndex)} 
-                                    data-index={weekIndex}
-                                >
-                                    {hoverID === getGridDate(dayIndex, weekIndex) &&
-                                        <div className="absolute top-0 rounded-md bg-surface-a10 text-xs text-white p-1 z-20">
-                                            <p>Today's date is {getGridDate(dayIndex, weekIndex)}</p>
+                            { Array.from({length: 53}, (_, weekIndex) => {
+                                const date = getGridDate(dayIndex, weekIndex)
+                                console.log("DATE FROM COMPONENT", date)
+
+                                let contributions = 0
+                                
+                                //move this to hover component? Does it matter? 
+                                monthlyHabitData[0].dailyProgress.forEach( obj => { 
+                                    if(obj.date === date) contributions =+ obj.habit.length
+                                })
+
+                                return(
+                                    <td 
+                                        className={ date.slice(-4) !== '2024' ? "bg-surface-a0" : "w-3 bg-surface-a10 rounded-sm table-cell border-separate"}
+                                        onMouseEnter={() =>  date.slice(-4) === '2024' && displayProgressInfo(date)}
+                                        onMouseLeave={restetProgressInfo}
+                                        key={weekIndex} 
+                                        id={`${dayIndex}-${weekIndex}`}  
+                                        data-date={date} 
+                                        data-index={weekIndex}
+                                    >
+                                        <div className="flex items-center justify-center relative">
+                                            {hoverID === date &&
+                                                <div className="absolute top-[-40px] rounded-md bg-surface-a20 text-xs text-white text-center p-2 z-20 flex">
+                                                    <p className="whitespace-nowrap">
+                                                        {contributions > 0 
+                                                            ? `${contributions} contributions were made on ${date}`
+                                                            : `No contributions made on ${date}`}
+                                                    </p>
+                                                </div>
+                                            }
                                         </div>
-                                    }
-                          
-                                </td>
-                            ))}
+                                    </td>
+                                )
+                            })}
                         </tr>
                     )
                 })} 
